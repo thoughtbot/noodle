513a3d446a2cf0f94959ea6241ebc055dbaf476b
patrick brisbin <pbrisbin@gmail.com>
2013-12-12 14:04:32 -0500

    Accept limit as argument

--- main.hs
+++ main.hs
@@ -3,19 +3,24 @@ module Main where
 import Noodle.Costs
 import Data.List (sortBy)
 import Data.Function (on)
+import System.Environment (getArgs)
 
 main :: IO ()
 main = do
     lns <- fmap lines getContents
 
-    mapM_ putStrLn $ map prettyPrint $ topTen $ costs $ map toInvocation lns
+    args <- getArgs
+
+    let n = read $ if null args then "10" else head args
+
+    mapM_ putStrLn $ map prettyPrint $ top n $ costs $ map toInvocation lns
 
 toInvocation :: String -> Invocation
 toInvocation = last . take 2 . words
 
-topTen :: [InvocationWithCost] -> [InvocationWithCost]
-topTen = take 50 . reverse
-       . sortBy (compareCosts `on` snd)
+top :: Int -> [InvocationWithCost] -> [InvocationWithCost]
+top n = take n . reverse
+      . sortBy (compareCosts `on` snd)
 
 prettyPrint :: InvocationWithCost -> String
 prettyPrint (i,c) = i ++ ": " ++ show (weight c)
