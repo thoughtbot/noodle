aaf15e7b4665b7e849460c13dd948a3188f89a91
patrick brisbin <pbrisbin@gmail.com>
2013-12-12 17:01:49 -0500

    Simplify

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,7 +1,30 @@
 module Main where
 
-import Noodle.Costs
+import Data.Function (on)
+import Data.List (sortBy)
 import System.Environment (getArgs)
+
+import qualified Data.Map as M
+
+parse :: String -> [String]
+parse = map (lastOr "" . take 2 . words) . lines
+    where
+        lastOr v [] = v
+        lastOr _ xs = last xs
+
+costs :: [String] -> [(String, Int)]
+costs commands = M.toList . M.mapWithKey toWeight
+               $ M.fromListWith (+) [(command, 1) | command <- commands]
+
+    where
+        toWeight :: String -> Int -> Int
+        toWeight command count = count * (length command)
+
+top :: Int -> [(String, Int)] -> [(String, Int)]
+top n = take n . reverse . sortBy (compare `on` snd)
+
+prettyPrint :: (String, Int) -> String
+prettyPrint (command, weight) = command ++ ": " ++ show weight
 
 main :: IO ()
 main = do
@@ -11,9 +34,5 @@ main = do
     mapM_ putStrLn $ map prettyPrint $ top lim $ costs commands
 
     where
-        readFirstOr v []     = v
-        readFirstOr _ (x:xs) = read x
-
-        parse = map (last . take 2 . words) . lines
-
-        prettyPrint (command,cost) = command ++ ": " ++ show (weight cost)
+        readFirstOr v []    = v
+        readFirstOr _ (x:_) = read x
