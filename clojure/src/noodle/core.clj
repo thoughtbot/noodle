318569056c929a1ebba4ccf8e758e08e73caa2c4
Ben Orenstein <ben.orenstein@gmail.com>
2013-12-12 16:19:57 -0500

    Add pretty printing and sorting

--- clojure/src/noodle/core.clj
+++ clojure/src/noodle/core.clj
@@ -15,3 +15,18 @@
 (defn weights
   [counts]
   (reduce weight {} counts))
+
+(defn print-one
+  [[command weight]]
+  (format "%s: %d\n" command weight))
+
+(defn pretty-print
+  [m]
+  (clojure.string/join (map print-one m)))
+
+(defn sort-by-weight
+  [m]
+  (into (sorted-map-by (fn [key1 key2]
+                         (compare [(get m key2) key2]
+                                  [(get m key1) key1])))
+        m))
--- clojure/test/noodle/core_test.clj
+++ clojure/test/noodle/core_test.clj
@@ -14,3 +14,11 @@
 (deftest weight-test
   (testing "Returns a map of weighted scores of badness"
     (is (= { "cd" 4 } (weights { "cd" 2 })))))
+
+(deftest printer
+  (testing "Pretty-printing the commands with their weights"
+    (is (= "cd: 4\nrake: 3\nzsh: 1\n" (pretty-print { "zsh" 1 "rake" 3 "cd" 4 })))))
+
+(deftest sorter
+  (testing "Sorting the results by weight"
+    (is (= (sorted-map "zsh" 4 "cd" 2 ) (sort-by-weight { "cd" 2 "zsh" 4 })))))
