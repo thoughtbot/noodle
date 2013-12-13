ed73ea511fcc358e3919009b761cd7e53370b62a
patrick brisbin <pbrisbin@gmail.com>
2013-12-13 10:16:43 -0500

    More haskell refactor

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,32 +1,19 @@
 module Main where
 
 import Data.Function (on)
-import Data.List (sortBy)
+import Data.List (sort)
 import System.Environment (getArgs)
 
 import qualified Data.Map as M
 
-data CommandCount = CommandCount String Int
+data CommandCount = CommandCount String Int deriving Eq
 
 instance Show CommandCount where
     show (CommandCount command count) =
         command ++ ": " ++ show count ++ " times"
 
-parse :: String -> [String]
-parse = map (lastOr "" . take 2 . words) . lines
-    where
-        lastOr v [] = v
-        lastOr _ xs = last xs
-
-counts :: [String] -> [CommandCount]
-counts commands = map (uncurry CommandCount) $ M.toList
-                $ M.fromListWith (+) [(command, 1) | command <- commands]
-
-weight :: CommandCount -> Int
-weight (CommandCount command count) = count * (length command)
-
-top :: Int -> [CommandCount] -> [CommandCount]
-top n = take n . reverse . sortBy (compare `on` weight)
+instance Ord CommandCount where
+    compare = compare `on` weight
 
 main :: IO ()
 main = do
@@ -37,3 +24,24 @@ main = do
     where
         readFirstOr v []    = v
         readFirstOr _ (x:_) = read x
+
+parse :: String -> [String]
+parse = map (lastOr "" . take 2 . words) . lines
+    where
+        lastOr v [] = v
+        lastOr _ xs = last xs
+
+counts :: [String] -> [CommandCount]
+counts = map (uncurry CommandCount) . M.toList . countsMap
+
+weight :: CommandCount -> Int
+weight (CommandCount command count) = count * (length command)
+
+top :: Ord a => Int -> [a] -> [a]
+top n = take n . reverse . sort
+
+countsMap :: (Num a, Ord k) => [k] -> M.Map k a
+countsMap = foldr increment M.empty
+
+increment :: (Num a, Ord k) => k -> M.Map k a -> M.Map k a
+increment c = M.insertWith (+) c 1
